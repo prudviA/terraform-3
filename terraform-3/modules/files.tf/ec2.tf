b0806a5a316461297e96a39c6c259c02ad9dc9e7
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 17:34:52 +0530

    Update ec2.tf

--- terraform-3/modules/files.tf/ec2.tf
+++ terraform-3/modules/files.tf/ec2.tf
@@ -1,14 +1,25 @@
-resource "aws_instance" "terraform-3" {
+resource "aws_instance" "terraform_3" {
   ami           = "ami-05c179eced2eb9b5b"
   instance_type = "t2.micro"
   subnet_id   = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     tags = {
-    Name = "terraform-3"
+    Name = "terraform_3"
   }
 }
 output "ec2_public_ip" {
-    value = aws_instance.terraform-3
+    value = aws_instance.terraform_3.public_ip
+}
+
+resource "aws_launch_configuration" "app_lc" {
+  name          = "app_lc"
+  image_id      = "ami-05c179eced2eb9b5b" # Same AMI as EC2
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.ec2_sg.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "app_asg" {
@@ -16,8 +27,14 @@ resource "aws_autoscaling_group" "app_asg" {
   desired_capacity   = 2
   max_size           = 3
   min_size           = 1
-  vpc_zone_identifier = var.private_subnet.id
+  vpc_zone_identifier = var.private_subnet.ids
+  launch_configuration = aws_launch_configuration.app_lc.name
 
+  tag {
+    key                 = "Name"
+    value               = "app_asg_instance"
+    propagate_at_launch = true
+  }
 }
 
 output "app_asg" {
