f43dc77a757ddf79e7ca5c1150c885e0fa753946
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 16:01:10 +0530

    Update ec2.tf

--- modules/files.tf/ec2.tf
+++ modules/files.tf/ec2.tf
@@ -1,25 +1,25 @@
-resource "aws_instance" "terraform-3" {
+resource "aws_instance" "terraform_3" {
   ami           = "ami-05c179eced2eb9b5b"
   instance_type = "t2.micro"
-  subnet_id   = aws_subnet.public-subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2-sg.id]
+  subnet_id   = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     tags = {
-    Name = "terraform-3"
+    Name = "terraform_3"
   }
 }
 output "ec2_public_ip" {
-    value = aws_instance.terraform-3
+    value = aws_instance.terraform_3
 }
 
-resource "aws_autoscaling_group" "app-asg" {
+resource "aws_autoscaling_group" "app_asg" {
   availability_zones = ["ap-south-1a"]
   desired_capacity   = 2
   max_size           = 3
   min_size           = 1
-  vpc_zone_identifier = var.private-subnet.id
+  vpc_zone_identifier = var.private_subnet.id
 
 }
 
-output "app-asg" {
-    value = aws_autoscaling_group.app-asg.name 
+output "app_asg" {
+    value = aws_autoscaling_group.app_asg.name 
 }
