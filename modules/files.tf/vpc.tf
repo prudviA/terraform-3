c6b3d1dfad6bb16b92cef2423008ec057d91d064
prudviA <165309155+prudviA@users.noreply.github.com>
2025-03-24 16:06:03 +0530

    Update vpc.tf

--- modules/files.tf/vpc.tf
+++ modules/files.tf/vpc.tf
@@ -1,45 +1,45 @@
-resource "aws_vpc" "3-tier" {
+resource "aws_vpc" "3_tier" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "3-tier"
+    Name = "3_tier"
   }
 }
 
-resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.3-tier.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.3_tier.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "public_subnet"
   }
 }
 
-resource "aws_subnet" "private-subnet" {
-  vpc_id     = aws_vpc.3-tier.id
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.3_tier.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private_subnet"
   }
 }
 
-resource "aws_subnet" "private-subnet-2" {
-  vpc_id     = aws_vpc.3-tier.id
+resource "aws_subnet" "private_subnet-2" {
+  vpc_id     = aws_vpc.3_tier.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "private-subnet-2"
+    Name = "private_subnet-2"
   }
 }
 
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.3-tier.id
+  vpc_id = aws_vpc.3_tier.id
 
   tags = {
     Name = "igw"
@@ -47,7 +47,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.3-tier.id
+  vpc_id = aws_vpc.3_tier.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -59,18 +59,18 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public-subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 output "vpc_id" {
-    value = aws_vpc.3-tier_vpc.id
+    value = aws_vpc.3_tier_vpc.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public-subnet[*].id
+  value = aws_subnet.public_subnet[*].id
 }
 
-output "private-subnet_id" {
-    value = aws_subnet.private-sunets[*].id
+output "private_subnet_id" {
+    value = aws_subnet.private_sunets[*].id
 }
